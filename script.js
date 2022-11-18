b34974e1a672111f9666007fd58efe7942144e12
Jean <jean118877@hotmail.com>
2022-11-18 08:23:23 -0300

    exercicio completo

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function handleMove(val){
     if(screen.width > 500){
-        let header = document.querySelector(val);
-        header.style.transition = "0.5s";
-        header.style.backgroundColor = "#584747";
+        val.style.transition = "0.5s";
+        val.style.backgroundColor = "#584747";
     }
 }
 
 function handleOut(val){
     if(screen.width > 500){
-        let header = document.querySelector(val);
-        header.style.transition = "0.5s";
-        header.style.backgroundColor = "#4B3738";
+        val.style.transition = "0.5s";
+        val.style.backgroundColor = "#4B3738";
     }
 }
